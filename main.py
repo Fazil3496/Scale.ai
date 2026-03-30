e845d5696ff8a600981a94bb7d94ceecad5a0171
M.D FAZIL <fazzi7790@gmail.com>
2026-03-30 10:47:31 +0530

    Deployment fix: Explicitly exposing app for Vercel

--- main.py
+++ main.py
@@ -121,3 +121,7 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+    app = app
+
+    if __name__ == "__main__":
+        app.run(debug=True)
